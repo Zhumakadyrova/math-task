dc0fe82cf69cfe5974eb18f824cd9f38090c1a7c
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-17 22:07:45 +0600

    function if else

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function getLength(a) {
   return a.length;
 }
 console.log(getLength("My name is Nagima"));
+
+function num(a) {
+  if(a % 2 === 0) {
+    console.log( a + " This number is even.")
+  }
+  else if (a % 2 !== 0) {
+    console.log(a + " This number is odd.")
+  }
+}
+num(9);
+num(10);
+num(15);
