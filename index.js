78ee483489c29591a32b944b132ef7903a2958bc
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-17 21:49:31 +0600

    found a conus value with known radius and height

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function name(a) {
   console.log( "My name is " + a)
 }
 name("Nagima");
+
+function conus (height, radius) {
+  let v = (1/3 * 3.14) * height * radius **2
+  console.log("The valume of the conus " + v)
+}
+conus(2, 10);
