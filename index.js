baedbb2edd4be6d31fd3343aa08e99e3140d401a
Nagima Zhumakadyrova <zhumakadyrova7@gmail.com>
2023-02-18 17:49:57 +0600

    solve the task about  sumTo with for

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-// 1 task
+// 1. Напишите функцию, которая выводит ваше имя и фамилию.
+// functionName("Erjan", "Kydyrov") // My  name is Erjan Kydyrov
+
 function name(a) {
   console.log( "My name is " + a)
 }
 name("Nagima");
-// 2 task
+// 2. Напишите функцию, которая выводит объем конуса по заданным параметрам - высота (height) и радиус (radius).
+// functionName(height, radius) // Объем конуса равен ...
+
 function conus (height, radius) {
   let v = (1/3 * 3.14) * height * radius **2
   console.log("The valume of the conus " + v)
 }
 conus(2, 10);
-// 3 task
+//3. Напишите функцию, которая принимает строку и возвращает ее длину.
+// functionName(" ... ") // Длина строки равна ...
+
 function getLength(a) {
   return a.length;
 }
 console.log(getLength("My name is Nagima"));
-//  4 task
+
+
+//  4. Напишите функцию, которая принимает число и проверяет, четное оно или нечетное.
+// functionName(num) // Число - четное || Число - нечетное
+
 function num(a) {
   if(a % 2 === 0) {
     console.log( a + " This number is even.")
@@ -26,7 +36,11 @@ function num(a) {
 num(9);
 num(10);
 num(15);
-// task 5
+
+
+// 5. Напишите функцию, которая возвращает квадрат числа.
+// functionName(num) // Квадрат этого числа равен
+
 function getDegree(a) {
   console.log( a ** 2)
 }
@@ -37,34 +51,50 @@ function getDegree2(a) {
   return a ** 2;
 }
 console.log(getDegree2(5));
-// task 6
 
-// task 7
+
+// 6. Напишите функцию, которая возвращает корень числа.
+// functionName(num) // Корень этого числа равен
+
+// 7. Напишите функцию, которая возвращает периметр пятиугольника.
+// functionName(a, b, c, d, e) // Периметр - ...
+
 function getNum(a, b, c, d, e){
   return "This is " + (a + b + c+ d+ e);
 }
 console.log(getNum(5, 6, 8, 9, 1));
 
-// task 8
+
+// 8. Напишите цикл, который выводит каждое третье число в промежутке от 0 до 33.
+// 0 3 6 9 ...
 
 for (let i = 0; i <= 33; i +=3)
 console.log(i);
 
-// task9
+// 9. Напишите цикл, вычисляющий факториал числа 8. (8!)
+// ...
 
-// task 10
+
+// 9. Напишите цикл, вычисляющий факториал числа 8. (8!)
+// ...
+
+// 10. Напишите цикл, который выводит квадраты чисел от 1 до 10.
+// // 1 4 9 ...
 for( let a =0; a <= 10; a ++){
     console.log("Square of " + a + " equal to " + a*a)
   }
 
-//   // task 11
-// function sumTo() {
-//   let sum =0;
-//   for (let i= 0; i <= 25; i ++) {
-//     sum += i;
-//   }
-// }
-// console.log(sumTo(25));
+  // 11. Напишите цикл, который вычисляет сумму чисел от 1 до 25.
+// ... 
+
+function sumTo(n) {
+  let sum =1;
+  for (let i= 1; i <= n; i ++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumTo(25));
 
 // function evenNumber(n) {
 //   if (n % 2 === 0);
@@ -117,17 +147,17 @@ let names = ["Nagima", "Nazima", "Azima", "Ayana", "Nuraiym"] ;
 
   let animals = ["cat", "dog", "snake", "lion"];
   for(let i = 0; i < animals.length; i ++) {
-    function animals2(a) {
-      if (a === animals [i]) {
-        console.log("true");
+    function animals2([a]) {
+      if (animals2 [a] === animals [i]) {
+        console.log(animals2 [a] - true);
       }
       else {
-        console.log("false")
+        console.log(animals2[a] - false)
       }
     }
   }  
-  animals2("cat");
-  animals2("dog");
+  animals2(["cat"]);
+  animals2(["dog"]);
 
 
 
